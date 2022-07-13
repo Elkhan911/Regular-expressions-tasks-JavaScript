4305192b08b8e002a40162fa9ec8f0ff6a7b0fca
Elkhan <955pro@mail.ru>
2022-07-13 23:18:32 +0500

    fix

--- script.js
+++ script.js
@@ -3,8 +3,28 @@ let pTwo = document.querySelector("#pTwo");
 let pThree = document.querySelector("#pThree");
 let button1 = document.querySelector("#button1");
 let button2 = document.querySelector("#button2");
+let button3 = document.querySelector("#button3");
 
-pOne.addEventListener("click", function () {
-  let res = pOne.innerHTML;
-  pOne.innerHTML = Number(res) + 1;
+button1.addEventListener("click", function () {
+  pOne.innerHTML = "<b>pOne.innerHTML</b>";
+});
+
+// pTwo.addEventListener("click", function () {
+//   pTwo.innerHTML = "!" + pTwo.innerHTML;
+// });
+
+// pOne.addEventListener("click", function () {
+//   pOne.innerHTML = Math.pow(pOne, 2);
+// });
+
+// button1.addEventListener("click", function () {
+//   pOne.innerHTML = "<b>жирный текст</b>";
+// });
+
+// button3.addEventListener("click", function () {
+//   console.log(pTwo.innerHTML);
+// });
+
+pTwo.addEventListener("click", function () {
+  pTwo.innerHTML = "<i>hello</i>";
 });

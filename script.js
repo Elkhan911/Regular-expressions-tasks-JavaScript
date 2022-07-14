34146eedfe84df0aa1e5b16123e5ac7856940c98
Elkhan <955pro@mail.ru>
2022-07-14 18:25:45 +0500

    fix

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+let button3 = document.querySelector("#button3");
+let link = document.querySelector("#link");
+let submitButton = document.querySelector("#buttonSub");
+let image = document.querySelector("#image");
+let divSector = document.querySelector("#div-sector");
+let button2 = document.querySelector("#button2");
 let pOne = document.querySelector("#pOne");
 let pTwo = document.querySelector("#pTwo");
 let pThree = document.querySelector("#pThree");
-let button1 = document.querySelector("#button1");
-let button2 = document.querySelector("#button2");
-let button3 = document.querySelector("#button3");
+let elem1 = document.querySelector("#elem1");
+let elem2 = document.querySelector("#elem2");
+let elem3 = document.querySelector("#elem3");
 
-let elem = document.querySelector("#elem");
-let submitButton = document.querySelector("#buttonSub");
-
-submitButton.addEventListener("click", function () {});
+//
+//************************************************************************* */
+//                              VARIABLES
+//************************************************************************* */
+//

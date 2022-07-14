c0de73f18379506f386e51adc1e4c8e36b68e0bc
Elkhan <955pro@mail.ru>
2022-07-14 14:09:10 +0500

    fix

--- script.js
+++ script.js
@@ -5,26 +5,7 @@ let button1 = document.querySelector("#button1");
 let button2 = document.querySelector("#button2");
 let button3 = document.querySelector("#button3");
 
-button1.addEventListener("click", function () {
-  pOne.innerHTML = "<b>pOne.innerHTML</b>";
-});
+let elem = document.querySelector("#elem");
+let submitButton = document.querySelector("#buttonSub");
 
-// pTwo.addEventListener("click", function () {
-//   pTwo.innerHTML = "!" + pTwo.innerHTML;
-// });
-
-// pOne.addEventListener("click", function () {
-//   pOne.innerHTML = Math.pow(pOne, 2);
-// });
-
-// button1.addEventListener("click", function () {
-//   pOne.innerHTML = "<b>жирный текст</b>";
-// });
-
-// button3.addEventListener("click", function () {
-//   console.log(pTwo.innerHTML);
-// });
-
-pTwo.addEventListener("click", function () {
-  pTwo.innerHTML = "<i>hello</i>";
-});
+submitButton.addEventListener("click", function () {});

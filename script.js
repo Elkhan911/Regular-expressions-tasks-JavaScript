4b9ba8aade579f656dc9fe4f7ceeaabbb4be7964
Elkhan911 <955pro@mail.ru>
2022-07-19 00:21:49 +0500

    fix

--- script.js
+++ script.js
@@ -4,17 +4,34 @@ let divSector = document.querySelector("#div-sector");
 let www = document.querySelectorAll(".www");
 let pTwo = document.querySelector("#pTwo");
 let pThree = document.querySelector("#pThree");
-let pOne = document.querySelector("#pOne");
 let elem1 = document.querySelector("#elem1");
 let elem2 = document.querySelector("#elem2");
 let elem3 = document.querySelector("#elem3");
+let elem4 = document.querySelector("#elem4");
 let allInputs = document.querySelectorAll(".inputs");
 let allP = document.querySelectorAll(".newP");
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
+let button3 = document.querySelector("#button3");
 let divParent = document.querySelector(".parent");
+let pOne = document.querySelector("#pOne");
+let parent = document.querySelector("#parent");
+
 //
 //************************************************************************* */
 //                              VARIABLES
 //************************************************************************* */
 //
 
-console.log(divParent);
+elem1.addEventListener("blur", func);
+
+function func() {
+  let str = elem1.value;
+  if (str.includes("3")) {
+    console.log(true);
+    return;
+  } else {
+    console.log(false);
+    return;
+  }
+}

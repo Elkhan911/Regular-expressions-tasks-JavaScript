463b06ea4b05a83cabfa2ce73a99d0a36f16239e
Elkhan911 <955pro@mail.ru>
2022-08-13 12:58:29 +0500

    fix

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 let input1 = document.querySelector("#input1");
-let taskAnswer1 = document.querySelector("#taskAnswer1");
-let taskAnswer2 = document.querySelector("#taskAnswer2");
+let taskAnswer11 = document.querySelector("#taskAnswer1");
+let taskAnswer12 = document.querySelector("#taskAnswer2");
+
+let inputLength = document.querySelector("#inputLength");
+let inputWidth = document.querySelector("inputWidth");
+let taskAnswer21 = document.querySelector("#taskAnswer21");
+let taskAnswer22 = document.querySelector("#taskAnswer22");
+let buttton1 = document.querySelector("#button1");
 
 input1.addEventListener("blur", function () {
-  taskAnswer1.textContent =
-    "Периметр квадрата:" + "  " + Number(input1.value) * 4;
+  //   if (Number(input1.value) !== "number") {
+  //     alert("Нужно ввести число");
+  //     input1.value = "";
+  //   }
+  //   if (Number(input1.value) == "number") {
+  //     taskAnswer11.textContent =
+  //       "Периметр квадрата:" + "  " + Number(input1.value) * 4;
 
-  taskAnswer2.textContent =
-    "Площадь квадрата:" + "  " + Number(input1.value) ** 2;
+  //     taskAnswer12.textContent =
+  //       "Площадь квадрата:" + "  " + Number(input1.value) ** 2;
+  //   }
+
+  console.log(typeof Number(input1.value));
 });

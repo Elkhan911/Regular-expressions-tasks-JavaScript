16852342ae797c344a9b8d076e466d7e8d6ee80c
Elkhan911 <955pro@mail.ru>
2022-09-09 22:54:03 +0500

    task2

--- script.js
+++ script.js
@@ -5,7 +5,15 @@
 // let str = 'ahb acb aeb aeeb adcb axeb';
 // Напишите регулярку, которая найдет строки 'ahb', 'acb', 'aeb' по шаблону: буква 'a', любой символ, буква 'b'.
 
-let str = "ahb acb aeb aeeb adcb axeb";
-let a = str.replace(/a.b/g, "!");
+let str1 = "ahb acb aeb aeeb adcb axeb";
+let a = str1.replace(/a.b/g, "!");
 
 console.log(a);
+
+// Дана строка:
+// let str = 'aba aca aea abba adca abea';
+// Напишите регулярку, которая найдет строки 'abba', 'adca', 'abea' по шаблону: буква 'a', 2 любых символа, буква 'a'.
+
+let str2 = "aba aca aea abba adca abea";
+let b = str2.replace(/a..a/g, "!");
+console.log(b);

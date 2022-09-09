2206136bfc7a44cd6c1a66432413b61776414298
Elkhan911 <955pro@mail.ru>
2022-09-09 23:42:15 +0500

    task 5

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-// Дана строка:
+// *****************************************************************************
+// ***********************   Регулярные выражения ******************************
+// *****************************************************************************
+
+//************************   Введение в регулярные выражения в JavaScript   *************************************
+
+// 1. Дана строка:
 // let str = 'ahb acb aeb aeeb adcb axeb';
 // Напишите регулярку, которая найдет строки 'ahb', 'acb', 'aeb' по шаблону: буква 'a', любой символ, буква 'b'.
 
 let str1 = "ahb acb aeb aeeb adcb axeb";
-let a = str1.replace(/a.b/g, "!");
-console.log(a);
+let res1 = str1.replace(/a.b/g, "!");
+console.log(res1);
 
-// Дана строка:
+// 2. Дана строка:
 // let str = 'aba aca aea abba adca abea';
 // Напишите регулярку, которая найдет строки 'abba', 'adca', 'abea' по шаблону: буква 'a', 2 любых символа, буква 'a'.
 
 let str2 = "aba aca aea abba adca abea";
-let b = str2.replace(/a..a/g, "!");
-console.log(b);
+let res2 = str2.replace(/a..a/g, "!");
+console.log(res2);
 
-// Дана строка:
+// 3. Дана строка:
 // let str = 'aba aca aea abba adca abea';
 // Напишите регулярку, которая найдет строки 'abba' и 'abea', не захватив 'adca'.
 
 let str3 = "aba aca aea abba adca abea";
-let c = str3.replace(/ab.a/g, "!");
-console.log(c);
+let res3 = str3.replace(/ab.a/g, "!");
+console.log(res3);
+
+//********************   Операторы повторения символов в регулярках   **********************************************
+
+// 4. Дана строка:
+// let str = 'aa aba abba abbba abca abea';
+// Напишите регулярку, которая найдет строки 'aba', 'abba', 'abbba' по шаблону:
+// буква 'a', буква 'b' любое количество раз, буква 'a'.
+
+let str4 = "aa aba abba abbba abca abea";
+let res4 = str4.replace(/ab+a/g, "!");
+console.log(res4);

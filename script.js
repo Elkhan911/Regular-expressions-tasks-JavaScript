413d5913274edc6962109af071a566688e4bf2cc
Elkhan911 <955pro@mail.ru>
2022-09-09 22:56:40 +0500

    task 3

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// Введение в регулярные выражения в JavaScript
-// Напишите регулярку, которая найдет строки 'ahb', 'acb', 'aeb' по шаблону: буква 'a', любой символ, буква 'b'
-
 // Дана строка:
 // let str = 'ahb acb aeb aeeb adcb axeb';
 // Напишите регулярку, которая найдет строки 'ahb', 'acb', 'aeb' по шаблону: буква 'a', любой символ, буква 'b'.
 
 let str1 = "ahb acb aeb aeeb adcb axeb";
 let a = str1.replace(/a.b/g, "!");
-
 console.log(a);
 
 // Дана строка:
@@ -17,3 +13,11 @@ console.log(a);
 let str2 = "aba aca aea abba adca abea";
 let b = str2.replace(/a..a/g, "!");
 console.log(b);
+
+// Дана строка:
+// let str = 'aba aca aea abba adca abea';
+// Напишите регулярку, которая найдет строки 'abba' и 'abea', не захватив 'adca'.
+
+let str3 = "aba aca aea abba adca abea";
+let c = str3.replace(/ab.a/g, "!");
+console.log(c);
